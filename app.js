484d8fb540e4a98840b2e5af86ef58dc8b961a29
Lohith <pavanlohith006@gmail.com>
2025-10-08 19:52:50 +0530

    comment

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const connectDB=require("./db/db.js")
 const jwt=require('jsonwebtoken')
 const post=require("./models/post.js")
 const middleware=require("./middleware.js")
+const Comment = require("./models/comment.js")
+
+
 const { v4: uuidv4 } = require('uuid');
 const user=require("./models/user.js")
+const comment = require('./models/comment.js')
 connectDB();
 app.use(express.json())
 //user routes
@@ -62,6 +66,7 @@ app.get("/api/user/profile",middleware,async(req,res)=>{
 app.post("/api/posts",async(req,res)=>{
     const{userid,caption,likes,created_date}=req.body;
     const newpost=new post({
+        ID:uuidv4(),
         userid,
         caption,
         likes,
@@ -94,6 +99,43 @@ app.patch("/api/posts/likes/:id",async(req,res)=>{
 app.delete("/api/posts/:id",async(req,res)=>{
       const id=req.params.id;
      const x=await post.findOneAndDelete({"userid":id});
+     res.send(x);
+
+})
+//comment part
+app.post("/api/comment",async(req,res)=>{
+    const{postid,comment,commented_date}=req.body;
+    const newcmt=new Comment({
+        postid,
+        comment,
+        commented_date,
+    })
+    const info=await newcmt.save();
+    res.send(info)
+
+})
+app.get("/api/Comment/:id",async(req,res)=>{
+      const id=req.params.id;
+    const x=await Comment.find({"postid":id});
+    res.send(x);
+
+
+})
+app.patch("/api/comment/update/:id",async(req,res)=>{
+    try{
+           const id=req.params.id;
+          const x=await Comment.findOneAndUpdate({"postid":id},req.body);
+         res.send(x);
+
+    }
+    catch(e)
+    {
+        res.send(e);
+    }
+})
+app.delete("/api/comment/delete/:id",async(req,res)=>{
+      const id=req.params.id;
+     const x=await Comment.findOneAndDelete({"postid":id});
      res.send(x);
 
 })
--- models/comment.js
+++ models/comment.js
@@ -1,1 +1,13 @@
-const mongoose=require('')
+const mongoose=require('mongoose');
+const cmntschema=new mongoose.Schema({
+    comment:{
+        type:String,
+        required:true,
+    },
+    postid:String,
+
+    "commented_date":{
+        type:Date
+    }
+})
+module.exports=mongoose.model("Comment",cmntschema)
