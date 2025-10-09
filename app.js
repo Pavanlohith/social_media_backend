1734be931fc3535f40b27c9249728994e6d46689
Lohith <pavanlohith006@gmail.com>
2025-10-09 20:16:55 +0530

    authunticating while updating and deleting post

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const jwt=require('jsonwebtoken')
 const post=require("./models/post.js")
 const middleware=require("./middleware.js")
 const Comment = require("./models/comment.js")
-
+const bcrypt=require('bcrypt')
 
 const { v4: uuidv4 } = require('uuid');
 const user=require("./models/user.js")
@@ -85,23 +85,35 @@ app.get("/api/posts",async(req,res)=>{
     }
 })
 app.get("/api/posts/:id",async(req,res)=>{
+   
     const id=req.params.id;
+
     const x=await post.findOne({"userid":id});
     res.send(x);
 
 })
-app.patch("/api/posts/likes/:id",async(req,res)=>{
-    const id=req.params.id;
-     const x=await post.findOneAndUpdate({"userid":id},req.body);
+
+
+app.patch("/api/posts/:uid",middleware,async(req,res)=>{
+    const uid=req.params.uid;
+     const exist=await user.findById(req.user.id)
+        if(uid!=req.user.id)
+res.send("you arenot allowed to update")
+     const x=await post.findOneAndUpdate({"userid":uid},req.body);
      res.send(x);
 
 })
-app.delete("/api/posts/:id",async(req,res)=>{
-      const id=req.params.id;
-     const x=await post.findOneAndDelete({"userid":id});
+app.delete("/api/posts/:uid",middleware,async(req,res)=>{
+      const uid=req.params.uid;
+        const exist=await user.findById(req.user.id)
+        if(uid!=req.user.id)
+            res.send("you arenot allowed to delete")
+
+     const x=await post.findOneAndDelete({"userid":uid});
      res.send(x);
 
 })
+
 //comment part
 app.post("/api/comment",async(req,res)=>{
     const{postid,comment,commented_date}=req.body;
@@ -121,10 +133,13 @@ app.get("/api/Comment/:id",async(req,res)=>{
 
 
 })
-app.patch("/api/comment/update/:id",async(req,res)=>{
+app.patch("/api/comment/update/:pid",async(req,res)=>{
     try{
-           const id=req.params.id;
-          const x=await Comment.findOneAndUpdate({"postid":id},req.body);
+           const id=req.params.pid;
+             const exist=await user.findById(req.user.id)
+        if(pid!=req.user.id)
+res.send("you arenot allowed to update")
+          const x=await Comment.findOneAndUpdate({"postid":pid},req.body);
          res.send(x);
 
     }
@@ -133,9 +148,12 @@ app.patch("/api/comment/update/:id",async(req,res)=>{
         res.send(e);
     }
 })
-app.delete("/api/comment/delete/:id",async(req,res)=>{
-      const id=req.params.id;
-     const x=await Comment.findOneAndDelete({"postid":id});
+app.delete("/api/comment/delete/:pid",async(req,res)=>{
+      const pid=req.params.pid;
+      const exist=await user.findById(req.user.id)
+        if(pid!=req.user.id)
+res.send("you arenot allowed to delete")
+     const x=await Comment.findOneAndDelete({"postid":pid});
      res.send(x);
 
 })
